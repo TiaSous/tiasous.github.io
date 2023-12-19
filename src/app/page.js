0f23da74ba92d2292b58d65bc521c77d18d2c299
TiaSous <tiago.rodrigues2@eduvaud.ch>
2023-12-19 14:11:22 +0100

    feat: correct bug

--- src/app/page.js
+++ src/app/page.js
@@ -5,23 +5,25 @@ export default function Home() {
     <>
       <div className='grid grid-cols-7 h-full'>
         <div className='bg-red-300 col-start-1 col-end-4'></div>
-        <div className='col-start-4 col-end-8'>
-          <ul className='list-none flex justify-around items-center h-24'>
-            <li>Présentation</li>
-            <li>Projet</li>
-            <li>Avenir</li>
-            <li>Réseau</li>
-          </ul>
-          <div className='p-24 flex flex-col justify-center h-full'>
+        <div className='col-start-4 col-end-8 description'>
+          <nav className='flex items-center'>
+            <ul className='list-none flex justify-around items-center w-full'>
+              <li>Présentation</li>
+              <li>Projet</li>
+              <li>Avenir</li>
+              <li>Réseau</li>
+            </ul> 
+          </nav>
+          <div className='px-24 flex flex-col justify-center'>
             <h2 className='text-2xl mb-2'>Tiago Rodrigues Sousa</h2>
             <h3 className='text-xl mb-4'>Developper</h3>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-              magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+              magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
-          <div className='h-24'>
+          <div className=''>
 
           </div>
         </div>
